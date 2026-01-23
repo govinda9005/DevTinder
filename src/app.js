ff1a085970e59e950fd0a627d871bce6ea8deaef
govinda9005 <govindayadav9005@gmail.com>
2026-01-23 14:57:24 +0545

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,28 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user",(req,res) => {
-    res.send({ firstName:"Govinda", lastName:"Yadav"})
-})
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.post("/user",(req,res) => {
+app.use("/admin",adminAuth);  
+
+app.get("/user",userAuth, (req,res) => {
+    res.send("User data send")
+});
+
+app.get("/admin/getAllData",(req,res) => {
     //Savind Data to DB
-    res.send("Data saved successfuly")
+    res.send("All Data send")
 })
 
-app.delete("/user",(req,res) => {
-    res.send("Data deleted successfully")
+app.delete("/admin/deleteUser",(req,res) => {
+    res.send("User deleted successfully")
 })
-
-app.use("/test",(req, res) => {
-    res.send("Hello from the server 3000...!")
-});
-
-app.use("/",(req, res) => {
-    res.send("Welcome to Balen world!")
-});
-
-
 
 app.listen(3000, () => {
     console.log("Server is successfully lisrenning on port 3000....");
